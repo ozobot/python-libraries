0109f6a4e3cfe9f4ac9b895dba3d011045993834
Ondrej Novak <ondrej.novak@ozobot.com>
2025-05-15 14:58:24 +0200

    Add missing protocol tests

--- ozobot-protocol-common/src/ozobot/protocol_common/convert.py
+++ ozobot-protocol-common/src/ozobot/protocol_common/convert.py
@@ -1,21 +1,23 @@
+from __future__ import annotations
+
 import functools
 import operator
-from enum import IntEnum, IntFlag
-from typing import Any, List, Optional, Protocol, Type, Union, cast
+from typing import Any, Protocol
 
 
 class _Serializable(Protocol):
+    def __init__(self, val) -> None: ...
     def serialize(self) -> bytes: ...
 
 
-class ProtocolSizeError(RuntimeError):
-    def __init__(self, what, size, max_size):
-        super().__init__(
-            f"{what} size ({size}) exceeded maximal allowed size ({max_size}) by protocol"
-        )
+class _Deserializable(Protocol):
+    data_width: int
+
+    @classmethod
+    def deserialize(cls, data: bytes) -> _Deserializable: ...
 
 
-def asciiz2str(txt: Union[str, bytes]) -> str:
+def asciiz2str(asciiz: str | bytes) -> str:
     """Decodes ASCIIZ (C-string) to python string
 
     Function returns head of tring till first ``'\0'`` occourance. If ``'\0'`` not found,
@@ -26,14 +28,14 @@ def asciiz2str(txt: Union[str, bytes]) -> str:
 
     :return:       Python string extracted from ASCIIZ
     """
-    if isinstance(txt, str):
-        txt = txt.encode("ascii")
+    if isinstance(asciiz, str):
+        asciiz = asciiz.encode("ascii")
 
-    txt = txt.split(b"\x00", 1)[0]
-    return txt.decode()
+    asciiz = asciiz.split(b"\x00", 1)[0]
+    return asciiz.decode()
 
 
-def str2asciiz(txt: Union[str, bytes], size: Optional[int] = None) -> bytes:
+def str2asciiz(txt: str | bytes, size: int | None = None) -> bytes:
     """Encodes python string to ASCIIZ (C-string)
 
     Function returns python string as bytes with ``ASCII`` encoding terminated by ``b'\0'``.
@@ -59,43 +61,16 @@ def str2asciiz(txt: Union[str, bytes], size: Optional[int] = None) -> bytes:
     if length < size:
         txt += b"\0" * (size - length)
     elif length > size:
-        raise ProtocolSizeError(txt, length, size)
+        raise ValueError(
+            f"Provided string is larger than the limit: string {length}, limit: {size}"
+        )
 
     return txt
 
 
-def convert_to[T](expected_type: Type[T], value: Any) -> T:
-    """Process conversion of value to expected type
-
-    :param expected_type:    Target type
-    :param value:            Value to be converted to target type
-    """
-    # If we are expected type, just return us - always
-    if isinstance(value, expected_type):
-        return value
-
-    # If expected type is based on bytes (we are expecting that value is either string or not-iterable).
-    if issubclass(expected_type, bytes):
-        value = (
-            expected_type(value, "ascii")
-            if isinstance(value, str)
-            else expected_type([value])
-        )
-    # If converting bytes to str, use bytes.decode to avoid the b'' stringification.
-    elif issubclass(expected_type, str) and isinstance(value, bytes):
-        value = expected_type(value.decode("ascii"))
-    # If we are not bytes, then lets try to convert us directly
-    elif issubclass(expected_type, (IntEnum, IntFlag)):
-        value = expected_type(value)
-    elif isinstance(value, dict):
-        value = expected_type(**value)
-    else:
-        value = expected_type(value)  # type: ignore[call-arg]
-
-    return value
-
-
-def deserialize_array(member_type, data: bytes) -> Union[List, str, bytes]:
+def deserialize_array[T: _Deserializable](
+    member_type: type[str | T], data: bytes
+) -> str | list[T]:
     """Deserialize ``bytes`` to array of instances of requested type
 
     Takes bytes and creates array of items based on items sizes. If requested type is based on ``str``,
@@ -104,24 +79,23 @@ def deserialize_array(member_type, data: bytes) -> Union[List, str, bytes]:
     :param member_type:    Expected items type (see :py:class:`protocol_impl.Types`)
     :param data:           Bytes to be deserialized
     """
-    if issubclass(member_type, (bytes, str)):
-        # Convert ASCIIZ to str
+    if issubclass(member_type, str):
         return asciiz2str(data)
-    elif (
-        hasattr(member_type, "py_pack") and (member_type.py_pack == "B")
-    ) or issubclass(member_type, bytes):
+    elif hasattr(member_type, "py_pack") and member_type.py_pack == "B":  # type: ignore[attr-defined]
         # Array of bytes is always converted to bytes -> keep it as it is
-        return data
+        return data  # type: ignore[return-value]
     else:
         # The rest is converted to list of type based items
         return [
-            member_type.deserialize(data[r : r + member_type.data_width])
+            member_type.deserialize(data[r : r + member_type.data_width])  # type: ignore[misc]
             for r in range(0, len(data), member_type.data_width)
         ]
 
 
-def serialize_array[T: bytes | str | _Serializable](
-    array: List[T], member_type: Type[T], size: Optional[int] = None
+def serialize_array[T: _Serializable](
+    array: str | bytes | list[Any],
+    member_type: type[str | bytes | T],
+    size: int | None = None,
 ) -> bytes:
     """Serialize iterable of items to ``bytes``
 
@@ -133,16 +107,22 @@ def serialize_array[T: bytes | str | _Serializable](
                      when size does not match to serialized data.
     """
     if issubclass(member_type, (bytes, str)):
-        # Convert str to ASCIIZ
-        array_str_bytes = cast(Union[str, bytes], array)
-        data = str2asciiz(array_str_bytes, size)
+        if isinstance(array, str | bytes):
+            # Convert str to ASCIIZ
+            data = str2asciiz(array, size)
+        else:
+            raise ValueError(
+                "Only str and bytes types can be serialized to str and bytes"
+            )
     else:
         # The rest is converted from list of type based items
         data = functools.reduce(
-            operator.add, [convert_to(member_type, x).serialize() for x in array]
-        )  # type: ignore[attr-defined]
+            operator.add, [member_type(x).serialize() for x in array]
+        )
 
     if size is not None and len(data) > size:
-        raise ProtocolSizeError(data, len(data), size)
+        raise ValueError(
+            f"Expected array size does not match the real size, expected: {size}, real: {len(data)}"
+        )
 
     return data
---
+++ ozobot-protocol-common/tests/test_convert.py
@@ -0,0 +1,34 @@
+from ozobot.protocol_common import convert
+
+
+class _Serializable:
+    def __init__(self, val: int):
+        self._val = val
+
+    def serialize(self) -> bytes:
+        return bytes(range(self._val))
+
+
+def test_asciiz() -> None:
+    assert convert.asciiz2str(b"hello world\0") == "hello world"
+    assert convert.asciiz2str("hello world\0") == "hello world"
+
+    assert convert.str2asciiz("hello world") == b"hello world\0"
+    assert convert.str2asciiz("hello world") == b"hello world\0"
+    assert convert.str2asciiz("hello", 8) == b"hello\0\0\0"
+
+
+def test_serialize_bytes_str() -> None:
+    assert convert.serialize_array(b"hello", bytes, 8) == b"hello\0\0\0"
+    assert convert.serialize_array("hello", str, 8) == b"hello\0\0\0"
+
+
+def test_serialize_array() -> None:
+    assert (
+        convert.serialize_array([5, 2], _Serializable)
+        == b"\x00\x01\x02\x03\x04\x00\x01"
+    )
+
+
+def test_deserialize_array() -> None:
+    assert convert.deserialize_array(str, b"hello\0\0\0") == "hello"
