140fd96716d587d78dff734f1dcee014f6c330ad
Ondrej Novak <ondrej.novak@ozobot.com>
2025-08-18 17:04:11 +0200

    add ari user io functions

--- ozobot-ari/src/ozobot/ari/api/core.py
+++ ozobot-ari/src/ozobot/ari/api/core.py
@@ -1,4 +1,21 @@
+from ozobot.ari.driver.native import NativeDriver
 from ozobot.linefollower.api.core import LineFollower
+from ozobot.linefollower.datatypes import Color, Direction
 
 
-class Ari(LineFollower): ...
+class Ari(LineFollower):
+    _driver: NativeDriver
+
+    def __init__(self, driver: NativeDriver) -> None:
+        super().__init__(driver)
+
+    async def user_io_print(self, message: str) -> None:
+        await self._driver.user_io_print(message)
+
+    async def user_io_alert(self, message: str, cancellable: bool = False) -> None:
+        await self._driver.user_io_alert(message, cancellable=cancellable)
+
+    async def user_io_prompt[T: (str, float, int, bool, Color, Direction)](
+        self, message: str, _type: type[T], options: list[T], cancellable: bool = False
+    ) -> T:
+        return await self._driver.user_io_prompt(message, _type, options, cancellable=cancellable)
