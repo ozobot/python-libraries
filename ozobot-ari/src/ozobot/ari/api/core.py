2a16c22a11cd822708ba214b2343922591d715e4
Ondrej Novak <ondrej.novak@ozobot.com>
2025-08-19 16:03:34 +0200

    Make `cancellable` a kwarg in ari userio api

--- ozobot-ari/src/ozobot/ari/api/core.py
+++ ozobot-ari/src/ozobot/ari/api/core.py
@@ -12,10 +12,10 @@ class Ari(LineFollower):
     async def user_io_print(self, message: str) -> None:
         await self._driver.user_io_print(message)
 
-    async def user_io_alert(self, message: str, cancellable: bool = False) -> None:
+    async def user_io_alert(self, message: str, *, cancellable: bool = False) -> None:
         await self._driver.user_io_alert(message, cancellable=cancellable)
 
     async def user_io_prompt[T: (str, float, int, bool, Color, Direction)](
-        self, message: str, _type: type[T], options: list[T], cancellable: bool = False
+        self, message: str, _type: type[T], options: list[T], *, cancellable: bool = False
     ) -> T:
         return await self._driver.user_io_prompt(message, _type, options, cancellable=cancellable)
