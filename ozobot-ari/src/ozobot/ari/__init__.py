85ed0b41faf186d4c96d558b646e85ceeae8e7e4
Ondrej Novak <ondrej.novak@ozobot.com>
2026-01-06 14:01:31 +0100

    Export ari and evo sync api

--- ozobot-ari/src/ozobot/ari/__init__.py
+++ ozobot-ari/src/ozobot/ari/__init__.py
@@ -1,7 +1,10 @@
 from ozobot.ari.api.core import Ari
-from ozobot.ari.api.handle import AriHandle
+from ozobot.ari.api.handle import AriHandle, SyncAriHandle
+from ozobot.ari.api.sync import SyncAri
 
 __all__ = [
     "Ari",
     "AriHandle",
+    "SyncAri",
+    "SyncAriHandle",
 ]
--- ozobot-evo/src/ozobot/evo/__init__.py
+++ ozobot-evo/src/ozobot/evo/__init__.py
@@ -1,7 +1,10 @@
 from ozobot.evo.api.core import Evo
-from ozobot.evo.api.handle import EvoHandle
+from ozobot.evo.api.handle import EvoHandle, SyncEvoHandle
+from ozobot.evo.api.sync import SyncEvo
 
 __all__ = [
     "Evo",
     "EvoHandle",
+    "SyncEvoHandle",
+    "SyncEvo",
 ]
