29cc8cb33c41bacbe0f463dec659463e24d19a70
Ondrej Novak <ondrej.novak@ozobot.com>
2026-02-09 18:23:17 +0100

    remove `buffered_iterator` implementation

--- ozobot-linefollower/src/ozobot/linefollower/api/data_access.py
+++ ozobot-linefollower/src/ozobot/linefollower/api/data_access.py
@@ -2,12 +2,9 @@ from __future__ import annotations
 
 import asyncio
 import contextlib
-import sys
 import typing
 
-from loguru import logger
 from ozobot.common.broadcast import BroadcastManager
-from ozobot.common.exceptions import OzobotError
 from ozobot.linefollower.datatypes import Sample
 
 
@@ -82,79 +79,6 @@ class DataReadConstant[T]:
 
     async def read(self) -> T:
         return self._factory()
-
-
-# workaround for the web-python's Pyodide still having Python 3.12 which does not have `asyncio.Queue.shutdown`
-#     TODO: remove when web-python has Python 3.13
-if sys.version_info >= (3, 13):  # noqa: UP036
-    _ShuttableQueue = asyncio.Queue
-    _QueueShutDown = asyncio.QueueShutDown
-else:
-
-    class _QueueShutDown(Exception): ...
-
-    class _ShuttableQueue[T](asyncio.Queue[T]):
-        _shutdown_token = object()
-
-        def __init__(self) -> None:
-            self._shutdown = False
-            super().__init__()
-
-        def shutdown(self) -> None:
-            self.put_nowait(self._shutdown_token)
-
-        async def get(self) -> T:
-            if self._shutdown:
-                raise _QueueShutDown()
-
-            v = await super().get()
-            if v is self._shutdown_token:
-                self._shutdown = True
-                raise _QueueShutDown()
-
-            return v
-
-
-@contextlib.asynccontextmanager
-async def buffered_iterator[T](
-    unbuffered_iter: typing.AsyncIterator[T],
-) -> typing.AsyncIterator[typing.AsyncIterator[T]]:
-    """
-    An async context manager consuming an async iterator and yielding a new async iterator that can be consumed even after the former one is closed.
-
-    This is useful in cases where closing a (possibly infinite) iterator denotes an end of a lifetime of some task, but the buffered values
-    are required after that happens.
-    """
-    q = _ShuttableQueue[T]()
-
-    async def _queue_to_aiter() -> typing.AsyncIterator[T]:
-        while True:
-            try:
-                yield await q.get()
-            except _QueueShutDown:
-                return
-
-    async def _read_task() -> None:
-        async for r in unbuffered_iter:
-            await q.put(r)
-        q.shutdown()
-
-    try:
-        async with asyncio.TaskGroup() as tg:
-            t = tg.create_task(_read_task())
-            try:
-                yield _queue_to_aiter()
-            finally:
-                t.cancel()
-                q.shutdown()
-    except* OzobotError as e:
-        # propagate the inner exception from _read_task()
-        group = e.subgroup(OzobotError)
-        if group:
-            exceptions = group.exceptions
-            if len(exceptions) > 1:
-                logger.warning("Dropping exceptions", dropped_exception=exceptions[1:])
-            raise exceptions[0] from e
 
 
 class WatcherOutputContainer[T]:
--- ozobot-linefollower/tests/test_api/test_data_access.py
+++ ozobot-linefollower/tests/test_api/test_data_access.py
@@ -8,7 +8,6 @@ from ozobot.linefollower.api.data_access import (
     EventWatcher,
     EventWatcherQueue,
     WatcherOutputContainerRunner,
-    buffered_iterator,
 )
 from ozobot.linefollower.datatypes import Sample
 
@@ -78,36 +77,6 @@ async def test_watcher_proxy() -> None:
 
         assert (await anext(it_str)) == "hello"
         assert (await anext(it_str)) == "world"
-
-
-async def test_buffered_iterator_finite(subtests: pytest.Subtests) -> None:
-    """Test if a finite iterator gets exhausted"""
-
-    async def _it() -> typing.AsyncIterator[int]:
-        for i in range(4):
-            yield i
-
-    with subtests.test(msg="Close after exhauston"):
-        async with buffered_iterator(_it()) as buffered_it:
-            assert [v async for v in buffered_it] == list(range(4))
-
-    with subtests.test(msg="Close before exhaustion"):
-        async with buffered_iterator(_it()) as buffered_it:
-            await asyncio.sleep(0.1)  # let the background task process the messages
-        assert [v async for v in buffered_it] == list(range(4))
-
-
-async def test_buffered_iterator_infinite() -> None:
-    """Test if an infinite iterator does not block"""
-
-    async def _it() -> typing.AsyncIterator[int]:
-        yield 0
-        yield 1
-        await asyncio.Future()  # block forever to simulate infinite iterator
-
-    async with buffered_iterator(_it()) as buffered_it:
-        await asyncio.sleep(0.1)  # let the background task process the messages
-    assert [v async for v in buffered_it] == list(range(2))
 
 
 async def test_watcher_container_aiter(subtests: pytest.Subtests) -> None:
