28932c9392eb22927c363182a58eebdc9c38ac7c
Ondrej Novak <ondrej.novak@ozobot.com>
2026-06-26 16:51:05 +0200

    fix: smoketest does not propagate watcher exceptions

--- scripts/smoketest.py
+++ scripts/smoketest.py
@@ -120,7 +120,8 @@ async def read_watch_sensors(sensors):
                 print(f"read {name} not available")
 
             t = asyncio.create_task(_watch(name, sensor))
-            await user_io_alert("Confirm to continue")
+            i = asyncio.create_task(user_io_alert("Confirm to continue"))
+            await asyncio.wait([t, i], return_when=asyncio.FIRST_COMPLETED)
             t.cancel()
         else:
             print(f"Skipping {name} - not present")
